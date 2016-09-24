0fa0f9d7d7af672484baf7feda75e6a5033197dc
VaibhavAgarwalVA <vaiagarwal96@gmail.com>
2016-09-25 02:14:29 +0530

    added edit

--- src/edit_page.java
+++ src/edit_page.java
@@ -78,7 +78,7 @@ public class edit_page {
 		passwordField = new JPasswordField();
 		passwordField.setBounds(182, 166, 220, 19);
 		frame.getContentPane().add(passwordField);
-		passwordField.setText(main.vec.elementAt(ind).get_password());
+		passwordField.setText("Enter new or current password...");
 		
 		JButton btnAddMe = new JButton("Confirm and Edit !");
 		btnAddMe.addActionListener(new ActionListener() {
